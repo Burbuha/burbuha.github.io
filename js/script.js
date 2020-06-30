2c0fd26ab608299be9458922d97f21127f35cdf4
Burbuha <burbovskaja@gmail.com>
2020-06-30 21:15:52 +0300

    to-top

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+//to-top
+jQuery(function ($) {
+  var toTop = $(".to-top");
+  var body = $("html, body");
+
+  $(window).scroll(function () {
+    var winScrollTopValue = $(window).scrollTop();
+
+    if (winScrollTopValue > 800) {
+      toTop.fadeIn(600);
+    } else if (winScrollTopValue < 800) {
+      toTop.hide();
+    }
+  });
+
+  toTop.on("click", function (event) {
+    event.preventDefault(); //отменить действие ссылки по умолчанию
+    body.animate({ scrollTop: 0 }, 1200);
+  });
+});
+
 //gallery
 $(document).ready(function () {
   $("#lightgallery").lightGallery();
