2510306edff8b5ba7b692b513fb910b3a183a6c2
Burbuha <burbovskaja@gmail.com>
2020-07-03 17:10:43 +0300

    03.07.20

--- js/script.js
+++ js/script.js
@@ -17,7 +17,4 @@ jQuery(function ($) {
     event.preventDefault(); //отменить действие ссылки по умолчанию
     body.animate({ scrollTop: 0 }, 1200);
   });
-
-  //gallery
-  $("#lightgallery").lightGallery();
 });
